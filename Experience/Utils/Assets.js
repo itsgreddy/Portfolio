5dcf8d467451ea8596766fd6e966df6a8527786d
Harsha <81374435+Harshavardhan7678@users.noreply.github.com>
2022-10-09 04:07:56 +0300

    Importing assets

---
+++ Experience/Utils/Assets.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        name: "room",
+        type: "glbModel",
+        path: "/models/Portfolio Room.glb",
+    }
+    // {
+    //     name: "screen",
+    //     type: "videoTexture",
+    //     path: "/textures/blabla.mp4",
+    // }
+]
---
+++ Experience/Utils/Resources.js
@@ -0,0 +1,2 @@
+// This is to load all the resources and store them 
+// So, whenever we need access anything we access if from resources
