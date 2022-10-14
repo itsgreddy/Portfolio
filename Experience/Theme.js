1e3ba31945a621132ba9ca594ae48891272212d2
Harsha <81374435+Harshavardhan7678@users.noreply.github.com>
2022-10-15 00:18:08 +0300

    Updating dark theme

--- Experience/Theme.js
+++ Experience/Theme.js
@@ -16,6 +16,8 @@ export default class Theme extends EventEmitter {
         this.toggleButton.addEventListener("click", () => {
             this.toggleCircle.classList.toggle("slide"); // Calling Slide class and using it to tranfrom
             this.theme = this.theme === "light" ? "dark" : "light"; // Ternary operater | If theme is light set to dark or vice versa
+            document.body.classList.toggle("dark-theme");
+            document.body.classList.toggle("light-theme");
             // console.log(this.theme);
 
             this.emit("switch", this.theme); // Passing argument called this.theme
