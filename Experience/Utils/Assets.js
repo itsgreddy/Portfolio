23fd44d09fa62d2b28ea8d3101446e21837d96ea
Harsha <81374435+Harshavardhan7678@users.noreply.github.com>
2022-10-17 20:00:30 +0300

    Updating the model

--- Experience/Utils/Assets.js
+++ Experience/Utils/Assets.js
@@ -2,7 +2,7 @@ export default [
     {
         name: "room",
         type: "glbModel",
-        path: "/models/Final Version.glb",
+        path: "/models/Final Model.glb",
     },
     // {
     //     name: "screen",
