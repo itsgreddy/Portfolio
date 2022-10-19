a276a12101833c462f5f6615b781ea4bd11a9bc7
Harsha <81374435+Harshavardhan7678@users.noreply.github.com>
2022-10-19 23:17:03 +0300

    Creating fucntions for header spans

---
+++ Experience/Utils/convertDivsToSpan.js
@@ -0,0 +1,11 @@
+export default function (element) {
+    element.style.overflow = hidden; // To hide the translation from 100% to 0% on Y axis
+    element.innerHTML = element.innerText
+        .split("")
+        .map(char => { // Splitting and mapping
+            return `<span class = "animatedis"></span>`; // Passing into GSAP to use stagger
+        })
+        .join("");
+
+    return element;
+}
