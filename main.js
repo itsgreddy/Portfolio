29622fbae3545ca0001c23ce41faa120043adac9
Harsha <81374435+Harshavardhan7678@users.noreply.github.com>
2022-10-26 13:35:10 +0300

    Adding in scripts for About section

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ import Experience from './Experience/Experience'
 const experience = new Experience(document.querySelector(".experience-canvas")) // Passing the canvas element for ThreeJS to the class "Experience"
 
 const tabsContainer = document.querySelector(".tab-titles"),
-    aboutSection = document.querySelector(".second-section");
+    practicumSection = document.querySelector(".second-section");
 
 tabsContainer.addEventListener("click", (e) => {
     // console.log(e.target); // Shows in console whenever we click any of the sections
@@ -15,7 +15,21 @@ tabsContainer.addEventListener("click", (e) => {
 
         const target = e.target.getAttribute("data-target"); // Getting the ID
         // console.log(target);
+        practicumSection.querySelector(".tab-content.active").classList.remove("active");
+        practicumSection.querySelector(target).classList.add("active"); // using the target and adding the active
+    }
+})
+
+const aboutContainer = document.querySelector(".about-tabs"),
+    aboutSection = document.querySelector(".first-section");
+
+aboutContainer.addEventListener("click", (e) => {
+    if (e.target.classList.contains("tab-item") && !e.target.classList.contains("active")) {
+        aboutContainer.querySelector(".active").classList.remove("active");
+        e.target.classList.add("active");
+
+        const target = e.target.getAttribute("data-target");
         aboutSection.querySelector(".tab-content.active").classList.remove("active");
-        aboutSection.querySelector(target).classList.add("active"); // using the target and adding the active
+        aboutSection.querySelector(target).classList.add("active");
     }
 })
