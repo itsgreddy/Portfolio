a1fdca8c1115d600b505498abe78a14fb0499fa8
Harsha <81374435+Harshavardhan7678@users.noreply.github.com>
2022-10-26 12:22:22 +0300

    Initiaiting scripts for Practicum

--- main.js
+++ main.js
@@ -2,3 +2,10 @@ import './style.css'
 import Experience from './Experience/Experience'
 
 const experience = new Experience(document.querySelector(".experience-canvas")) // Passing the canvas element for ThreeJS to the class "Experience"
+
+const tabsContainer = document.querySelector(".tab-titles"),
+    aboutSection = document.querySelector(".second-section");
+
+tabsContainer.addEventListener("click", (e) => {
+    console.log(e.target); // Shows in console whenever we click any of the sections
+})
