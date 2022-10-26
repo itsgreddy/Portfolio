4e139a7b9b16cfad236450391d87b6e892fc3e10
Harsha <81374435+Harshavardhan7678@users.noreply.github.com>
2022-10-26 12:39:53 +0300

    Completing the scripts for Practicum

--- main.js
+++ main.js
@@ -9,8 +9,13 @@ const tabsContainer = document.querySelector(".tab-titles"),
 tabsContainer.addEventListener("click", (e) => {
     // console.log(e.target); // Shows in console whenever we click any of the sections
     if (e.target.classList.contains("tab-item") && !e.target.classList.contains("active")) {
-        tabsContainer.querySelector(".active").classList.remove("active");
+        tabsContainer.querySelector(".active").classList.remove("active"); // Changing active variable here
         e.target.classList.add("active");
-        console.log("I am working")
+        // console.log("I am working")
+
+        const target = e.target.getAttribute("data-target"); // Getting the ID
+        // console.log(target);
+        aboutSection.querySelector(".tab-content.active").classList.remove("active");
+        aboutSection.querySelector(target).classList.add("active"); // using the target and adding the active
     }
 })
